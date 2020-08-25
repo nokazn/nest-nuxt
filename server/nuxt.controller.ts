90da8ffcf240c596235c1f94fabe9daae6c71067
nokazn <nokazn@gmail.com>
2020-08-25 20:37:26 +0900

    update nuxt controller

--- server/nuxt.controller.ts
+++ server/nuxt.controller.ts
@@ -1,32 +1,27 @@
-import { Controller, Get, Request, Response } from "@nestjs/common";
+import { Controller, Get, Request, Response } from '@nestjs/common';
+import { Builder, Nuxt } from 'nuxt';
 
-import { Builder, Nuxt } from "nuxt";
-import * as config from "../nuxt.config.js";
+import { config } from '../nuxt.config.js';
 
 @Controller()
 export class NuxtController {
-  nuxt;
+  private nuxt: Nuxt;
 
   constructor() {
-    if (process.env.mode === "production") {
+    this.nuxt = new Nuxt(config);
+    if (process.env.mode === 'production') {
       config.dev = false;
-      this.nuxt = new Nuxt(config);
-    }
-    else if (process.env.IS_NUXT_ENABLED) {
+    } else if (process.env.IS_NUXT_ENABLED) {
       this.nuxt = new Nuxt(config);
       new Builder(this.nuxt).build();
     }
   }
 
   @Get('*')
-  async root(
-    @Request() req,
-    @Response() res
-  ) {
+  async root(@Request() req, @Response() res) {
     if (this.nuxt) {
       await this.nuxt.render(req, res);
-    }
-    else {
+    } else {
       res.send('Nuxt is disabled.');
     }
   }
